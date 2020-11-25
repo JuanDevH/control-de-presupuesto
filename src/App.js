27a99d634c7a4211013b6d20f6b87d21a883bb65
Juan Hernandez <hajjose7@gmail.com>
2020-11-25 15:17:22 +0100

    creando los estilos principales y el formulario

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import React from 'react';
+import Pregunta from './components/Pregunta'
+
 function App() {
   return (
-    <h1>Presupuesto</h1>
+    <div className="container">
+      <header>
+        <h1>Gasto Semanal</h1>
+
+        <div className="contenido-principal contenido">
+          <Pregunta />
+        </div>
+      </header>
+    </div>
   );
 }
 
