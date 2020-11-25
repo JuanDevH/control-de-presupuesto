537a371411b8e80aaffcc25ee10683c6ce73d5ab
Juan Hernandez <hajjose7@gmail.com>
2020-11-25 17:00:15 +0100

    Formulario de gastos y cantidad restante

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import Pregunta from './components/Pregunta'
+import React, { useState } from 'react';
+import Pregunta from './components/Pregunta';
+import Formulario from './components/Formulario';
 
 function App() {
+
+  // Definir el state
+  const [ presupuesto, guardarPresupuesto ] = useState(0);
+  const [ restante, guardarRestante ] = useState(0);
+
   return (
     <div className="container">
       <header>
         <h1>Gasto Semanal</h1>
 
         <div className="contenido-principal contenido">
-          <Pregunta />
+          <Pregunta 
+            guardarPresupuesto={guardarPresupuesto}
+            guardarRestante={guardarRestante}
+          />
+
+          <div className="row">
+            <div className="one-half column"> <Formulario /> </div>
+            <div className="one-half column">2</div>
+          </div>
         </div>
       </header>
     </div>
--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,47 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Error from './Error';
 
-const Pregunta = () => {
+const Pregunta = ({guardarPresupuesto, guardarRestante}) => {
+
+	// Definir el state
+	const [ cantidad, guardarCantidad ] = useState(0);
+	const [ error, guardarError ] = useState(false);
+
+	// Funcion que lee el presupuesto
+	const definirPresupuesto = e => {
+		guardarCantidad(parseInt(e.target.value, 10))
+	}
+
+	// Submit para definir el presupuesto
+	const agregarPresupuesto = e => {
+		e.preventDefault();
+
+		// Validar
+		if(cantidad < 1 || isNaN(cantidad) ) {
+			guardarError(true);
+			return;
+		}
+
+		// Si se pasa la validacion
+		guardarError(false);
+		guardarPresupuesto(cantidad);
+		guardarRestante(cantidad);
+	}
+
 	return (
 		<Fragment>
 			<h2>Coloca tu presupuesto</h2>
 
-			<form>
+			{ error ?  <Error mensaje="El Presupuesto es Incorrecto" /> : null }
+
+			<form
+				onSubmit={agregarPresupuesto}
+			>
 				<input 
 					type="number"
 					className="u-full-width"
 					placeholder="Coloca tu presupuesto"
+					onChange={definirPresupuesto}
 				/>
 
 				<input 
